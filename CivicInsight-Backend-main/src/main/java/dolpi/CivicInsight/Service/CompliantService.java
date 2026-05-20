02b570e3d443cbbdbc07554e1399b4442edc8889
Manas Rastogi <manasrastogi64@gmail.com>
2026-05-20 16:38:55 +0530

    Update CompliantService.java

--- CivicInsight-Backend-main/src/main/java/dolpi/CivicInsight/Service/CompliantService.java
+++ CivicInsight-Backend-main/src/main/java/dolpi/CivicInsight/Service/CompliantService.java
@@ -7,17 +7,18 @@ import dolpi.CivicInsight.Repository.ReportRepo;
 import dolpi.CivicInsight.Repository.UserRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.MediaType;
-import java.util.*;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
-public class  CompliantService {
+public class CompliantService {
 
     @Autowired
     private ReportRepo reportRepo;
@@ -32,22 +33,41 @@ public class  CompliantService {
     private EmailService emailService;
 
     @Autowired
-    private  GroqService groqService;
+    private GroqService groqService;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    //this method perform task @Async
     @Async
-    public void processComplaint(Complaints complaints) {
+    public void processComplaint(Complaints complaints,
+                                  GroqAnalysis cachedAnalysis,
+                                  String md5Key) {
 
-        GroqAnalysis analysis = groqService.analyzeComplaint(complaints.getComplaint());
+        GroqAnalysis analysis;
+
+        if (cachedAnalysis != null) {
+            log.info("✅ Cached analysis use - Groq skip!");
+            analysis = cachedAnalysis;
+
+        } else {
+            log.info("Groq call...");
+            analysis = groqService.analyzeComplaint(complaints.getComplaint());
+
+            redisTemplate.opsForValue().set(
+                    md5Key, analysis, Duration.ofHours(24)
+            );
+            log.info("save in the Redis: {}", md5Key);
+        }
 
         List<OfficerEnty> listofofficer = officerRepo.findByCityAndDepartment(
-                complaints.getCity(), analysis.getDepartment()  // ← getDepartment()
+                complaints.getCity(), analysis.getDepartment()
         );
 
         if (listofofficer == null || listofofficer.isEmpty()) {
-            log.error("No officer found for: {} - {}", complaints.getCity(), analysis.getDepartment());
+            log.error("No officer found for: {} - {}",
+                    complaints.getCity(), analysis.getDepartment());
             return;
         }
 
@@ -66,32 +86,33 @@ public class  CompliantService {
                 officerRepo.save(officer);
                 reportRepo.save(complaints);
 
-                Optional<UserEnity> optionalUser = userRepo.findById(complaints.getUserId());
+                Optional<UserEnity> optionalUser =
+                        userRepo.findById(complaints.getUserId());
+
                 if (optionalUser.isPresent()) {
                     UserEnity user = optionalUser.get();
                     emailService.sendEmail(
                             user.getEmail(),
                             "Complaint Successfully Submitted - CivicInsight",
                             "<h2>Hello " + user.getName() + "!</h2>" +
-                                    "<p>Aapki complaint successfully submit ho gayi hai.</p>" +
-                                    "<p><b>Complaint ID:</b> " + complaints.getId() + "</p>" +
-                                    "<p><b>Complaint:</b> " + complaints.getComplaint() + "</p>" +
-                                    "<p><b>Assigned Officer:</b> " + officer.getName() + "</p>" +
-                                    "<p><b>Department:</b> " + analysis.getDepartment() + "</p>" +
-                                    "<p><b>Category:</b> " + analysis.getCategory() + "</p>" +
-                                    "<p><b>Summary:</b> " + analysis.getSummary() + "</p>" +
-                                    "<p><b>Suggested Action:</b> " + analysis.getSuggested_Action() + "</p>" +
-                                    "<p><b>City:</b> " + complaints.getCity() + "</p>" +
-                                    "<p><b>Urgency:</b> " + analysis.getUrgency() + "</p>" +
-                                    "<p><b>Status:</b> Pending </p>" +
-                                    "<p>CivicInsight Team</p>"
+                            "<p>Aapki complaint successfully submit ho gayi hai.</p>" +
+                            "<p><b>Complaint ID:</b> " + complaints.getId() + "</p>" +
+                            "<p><b>Complaint:</b> " + complaints.getComplaint() + "</p>" +
+                            "<p><b>Assigned Officer:</b> " + officer.getName() + "</p>" +
+                            "<p><b>Department:</b> " + analysis.getDepartment() + "</p>" +
+                            "<p><b>Category:</b> " + analysis.getCategory() + "</p>" +
+                            "<p><b>Summary:</b> " + analysis.getSummary() + "</p>" +
+                            "<p><b>Suggested Action:</b> " + analysis.getSuggested_Action() + "</p>" +
+                            "<p><b>City:</b> " + complaints.getCity() + "</p>" +
+                            "<p><b>Urgency:</b> " + analysis.getUrgency() + "</p>" +
+                            "<p><b>Status:</b> Pending</p>" +
+                            "<p>CivicInsight Team</p>"
                     );
                 }
                 break;
             }
         }
     }
-
 
     public Complaints checkstatus(String id) {
         Optional<Complaints> complaint = reportRepo.findById(id);
